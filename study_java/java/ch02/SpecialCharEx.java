b7711f485e80571e384e8c8ae2fba73f8321819b
sohyeonnnn <limsohyeonn.18@gmail.com>
2022-07-28 15:14:02 +0900

    first commit

---
+++ study_java/java/ch02/SpecialCharEx.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class SpecialCharEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println('\''); // \를 앞에 붙이면 ' 출력
+		System.out.println("abc\t123\b456");
+		System.out.println('\n'); //개행
+		System.out.println("\"Hello\"");
+		System.out.println("c:\\");
+
+	}
+
+}
